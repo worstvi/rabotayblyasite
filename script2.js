3eeba86e477fd5de4c5ceb26d9969bf630abcddd
worstvi <157382005+worstvi@users.noreply.github.com>
2024-01-28 00:27:03 +0800

    script2.js

--- script2.js
+++ script2.js
@@ -1,24 +1,19 @@
-// script2.js
-function selectActivity(activity) {
-  alert(`Выбрано: ${activity}`);
-  
-  // Отправляем уведомление на сервер
-  sendNotificationToServer(activity);
-}
-
-function sendNotificationToServer(activity) {
-  const serverEndpoint = 'http://localhost:3000/sendNotification'; // Адрес вашего сервера
-
-  fetch(serverEndpoint, {
+function sendEmailNotification(activity) {
+  fetch('http://localhost:3000/send-notification', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({
-      activity,
-    }),
+    body: JSON.stringify({ activity }),
   })
-    .then(response => response.json())
-    .then(data => console.log('Ответ от сервера:', data))
-    .catch(error => console.error('Ошибка:', error));
+  .then(response => response.json())
+  .then(data => console.log('Серверный ответ:', data))
+  .catch(error => console.error('Ошибка отправки запроса:', error));
+}
+
+function selectActivity(activity) {
+  alert(`Выбрано: ${activity}`);
+  sendEmailNotification(activity);
+
+  // Здесь вы можете добавить код для дополнительных действий, связанных с выбором деятельности.
 }
