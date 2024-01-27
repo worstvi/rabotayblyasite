e422c2129ebf494603d6cad73f332eb9bfef34f5
worstvi <157382005+worstvi@users.noreply.github.com>
2024-01-28 02:05:30 +0800

    script2.js

--- script2.js
+++ script2.js
@@ -1,27 +1,27 @@
-document.addEventListener('DOMContentLoaded', () => {
-  function sendEmailNotification(activity) {
-    fetch('http://localhost:3000/send-notification', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ activity }),
+function sendEmailNotification(activity) {
+  fetch('http://localhost:3000/send-notification', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ activity }),
+    mode: 'cors',
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Ошибка HTTP: ${response.status}`);
+      }
+
+      return response.json();
     })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`Ошибка HTTP: ${response.status}`);
-        }
+    .then(data => console.log('Серверный ответ:', data))
+    .catch(error => console.error('Ошибка отправки запроса:', error));
+}
 
-        return response.json();
-      })
-      .then(data => console.log('Серверный ответ:', data))
-      .catch(error => console.error('Ошибка отправки запроса:', error));
-  }
+function selectActivity(activity) {
+  alert(`Выбрано: ${activity}`);
+  sendEmailNotification(activity);
+}
 
-  // Перемещаем функцию в глобальную область видимости
-  window.selectActivity = function (activity) {
-    alert(`Выбрано: ${activity}`);
-    sendEmailNotification(activity);
-    // Здесь вы можете добавить код для дополнительных действий, связанных с выбором деятельности.
-  };
+document.addEventListener('DOMContentLoaded', () => {
 });
