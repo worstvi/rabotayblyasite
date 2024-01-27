550b3087d1c6903181712ed2a45463db7b7b3092
worstvi <157382005+worstvi@users.noreply.github.com>
2024-01-28 00:26:52 +0800

    server.js

--- server.js
+++ server.js
@@ -1,48 +1,46 @@
-// server.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
 
 const app = express();
-const port = 3000;
+const port = 3000; // Выберите подходящий порт
 
 app.use(bodyParser.json());
 
-app.post('/sendNotification', (req, res) => {
+app.post('/send-notification', (req, res) => {
   const { activity } = req.body;
 
-  // Отправляем уведомление на почту
+  // Отправка уведомления на почту
   sendEmailNotification(activity);
 
-  console.log(`Получено уведомление: Пользователь выбрал: ${activity}`);
   res.json({ success: true });
-});
-
-app.listen(port, () => {
-  console.log(`Сервер запущен на порту ${port}`);
 });
 
 function sendEmailNotification(activity) {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: 'namlunsasha@gmail.com',
-      pass: 'namsa99221710',
+      user: 'namlunsasha@gmail.com', // Ваша почта
+      pass: 'namsa99221710', // Ваш пароль от почты
     },
   });
 
   const mailOptions = {
-    from: 'namlunsasha@gmail.com',
-    to: 'musasim@list.ru',
-    subject: 'Уведомление от сайта',
+    from: 'namlunsasha@gmail.com', // Ваша почта
+    to: 'musasim@list.ru', // Почта получателя
+    subject: 'Уведомление о выборе',
     text: `Пользователь выбрал: ${activity}`,
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
-      console.error('Ошибка отправки почты:', error);
+      console.error('Ошибка отправки уведомления:', error);
     } else {
       console.log('Уведомление отправлено:', info.response);
     }
   });
 }
+
+app.listen(port, () => {
+  console.log(`Сервер запущен на порту ${port}`);
+});
